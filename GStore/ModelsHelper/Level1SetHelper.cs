307dc76374b29fe84a83d8aa3e93174daf7ea355
ilia-presents <ilia.shterev.sk@gmail.com>
2025-07-11 00:40:50 +0300

    Corrections for Level1Set and Level1Set.

---
+++ GStore/ModelsHelper/Level1SetHelper.cs
@@ -0,0 +1,79 @@
+﻿using GStore.Models.ViewModels;
+using GStore.Models;
+using System.Linq.Expressions;
+
+namespace GStore.ModelsHelper
+{
+    public static class Level1SetHelper
+    {
+        public static void ToggleActivityStatus(Level1Set l1Set)
+        {
+            l1Set.IsActive = !l1Set.IsActive;
+        }
+
+        public static L1SetStutusChangeVM SetStatusChange(L1SetVM l1SetVM)
+        {
+            L1SetStutusChangeVM statusChangeVM = new L1SetStutusChangeVM();
+
+            statusChangeVM.l1SetVM = l1SetVM;
+
+            if (l1SetVM.IsActive)
+            {
+                statusChangeVM.TitleText = "Деактивиране на Категория Ниво 1";
+                statusChangeVM.WarningText = "Сигурен ли сте, че искате да деактивирате тази Категория Ниво 1 ?";
+                statusChangeVM.ButtonText = "Деактивирай";
+            }
+            else
+            {
+                statusChangeVM.TitleText = "Активиране на Категория Ниво 1";
+                statusChangeVM.WarningText = "";
+                statusChangeVM.ButtonText = "Активирай";
+            }
+
+
+            return statusChangeVM;
+        }
+
+        public static Level1Set MapVmToEntity(L1SetVM l1SetVM)
+        {
+            Level1Set l1Set = new Level1Set()
+            {
+                Id = l1SetVM.Id,
+                Name = l1SetVM.Name,
+                IsActive = l1SetVM.IsActive
+            };
+
+            return l1Set;
+        }
+
+        public static void MapEntityForEdit(Level1Set l1Set, L1SetVM l1SetVM)
+        {
+            l1Set.Name = l1SetVM.Name;
+        }
+
+        public static L1SetVM MapEntityToVm(Level1Set l1Set)
+        {
+            L1SetVM l1SetVM = new L1SetVM()
+            {
+                Id = l1Set.Id,
+                Name = l1Set.Name,
+                IsActive = l1Set.IsActive
+            };
+
+            return l1SetVM;
+        }
+
+        public static Expression<Func<Level1Set, L1SetVM>> LevelSetToVmSelector
+        {
+            get
+            {
+                return l1Set => new L1SetVM()
+                {
+                    Id = l1Set.Id,
+                    Name = l1Set.Name,
+                    IsActive = l1Set.IsActive
+                };
+            }
+        }
+    }
+}
